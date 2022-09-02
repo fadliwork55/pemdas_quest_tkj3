6800aa17bd17523b466ce5e7ed872ffa2e4fe585
fadliwork55 <110509608+fadliwork55@users.noreply.github.com>
2022-09-02 14:21:18 +0700

    Add files via upload

---
+++ Python5-tkj3.py
@@ -0,0 +1,29 @@
+'''Bangun datar
+    - Persegi
+    - Segitiga
+    - Jajargenjang
+    - Lingkaran'''
+
+# Rumus Persigi Sisi * Sisi
+
+sisi1 = 10
+sisi2 = 10
+luas_p = sisi1 * sisi2
+
+print("Luas persegi adalah", luas_p)
+
+# Rumus segitiga A * T / 2
+
+alas = 20
+tinggi = 10
+luas_s = alas * tinggi / 2
+
+print("Luas segitiga adalah", luas_s)
+
+
+# Program Dinamis
+
+print("\n1.Luas Persegi")
+sisi = int(input("Masukkan nilai sisi: "))
+luas_P = sisi * sisi
+print("Luas Persegi adalah", luas_P)
